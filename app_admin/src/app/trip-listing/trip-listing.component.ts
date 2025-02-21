c676ba10f8aa9f85f07d9064e3902745266876aa
Jaiden <jahomii.art@gmail.com>
2025-02-21 00:13:25 -0500

    Module 7 completed baseline

--- app_admin/src/app/trip-listing/trip-listing.component.ts
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -4,6 +4,7 @@ import { TripCardComponent } from '../trip-card/trip-card.component';
 import { trips } from '../data/trips';
 import { TripDataService } from '../services/trip-data.service';
 import { Trip } from '../models/trip';
+import { AuthenticationService } from '../services/authentication.service';
 
 import { Router } from "@angular/router";
 
@@ -23,7 +24,8 @@ export class TripListingComponent implements OnInit {
 
   constructor(
     private tripDataService: TripDataService,
-  private router: Router
+    private router: Router,
+    private authenticationService: AuthenticationService
 ) {
     console.log('trip-listing constructor');
   }
@@ -50,6 +52,11 @@ export class TripListingComponent implements OnInit {
       console.log('Error: ' + JSON.stringify(error, null, 2));
     }
   })
+}
+
+public isLoggedIn()
+{
+return this.authenticationService.isLoggedIn();
 }
 
   ngOnInit(): void {
